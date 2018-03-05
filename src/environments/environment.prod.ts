ec7d4fc146d36173429f007ede6f07c3474b003d
= <roman@muehlfeldner.net>
2018-03-05 15:38:14 +0100

    added firebase config to prod environment and installed ngrx/store

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCUxFt8S8PS1OKDpUeTrRNF0wo1Uu5JVrc',
+    authDomain: 'ng-mat-fitness-tracker.firebaseapp.com',
+    databaseURL: 'https://ng-mat-fitness-tracker.firebaseio.com',
+    projectId: 'ng-mat-fitness-tracker',
+    storageBucket: 'ng-mat-fitness-tracker.appspot.com',
+    messagingSenderId: '683363080800'
+  }
 };
