41f96b7e2c37d3a568833dd9358a6ac7e55e99a9
= <roman@muehlfeldner.net>
2018-03-05 12:08:45 +0100

    Refactoring

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,26 +1,37 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
 import { NgForm } from '@angular/forms';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/takeUntil';
 
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
   styleUrls: ['./new-training.component.scss']
 })
-export class NewTrainingComponent implements OnInit {
-  @Output() trainingStart = new EventEmitter<void>();
+export class NewTrainingComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<boolean>();
+
   exercises: Exercise[];
 
   constructor(public trainingService: TrainingService) {
   }
 
   ngOnInit() {
-    this.exercises = this.trainingService.getAvailableExercises();
+    this.trainingService.exercisesChange
+      .takeUntil(this.destroy$)
+      .subscribe((exercises: Exercise[]) => this.exercises = exercises);
+
+    this.trainingService.fetchAvailableExercises();
   }
 
   onStartTraining(form: NgForm): void {
     this.trainingService.startExercise(form.value['exercise']);
   }
 
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
 }
--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,25 +1,32 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
+import 'rxjs/add/operator/takeUntil';
+import { Subject } from "rxjs/Subject";
 
 @Component({
   selector: 'app-past-training',
   templateUrl: './past-training.component.html',
   styleUrls: ['./past-training.component.scss']
 })
-export class PastTrainingComponent implements OnInit, AfterViewInit {
+export class PastTrainingComponent implements OnInit, AfterViewInit, OnDestroy {
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
   dataSource = new MatTableDataSource<Exercise>();
-
   @ViewChild(MatSort) sort;
   @ViewChild(MatPaginator) paginator;
+  private destroy$ = new Subject<boolean>();
 
   constructor(private trainingService: TrainingService) {
   }
 
   ngOnInit() {
-    this.dataSource.data = this.trainingService.getAllExercises();
+    this.trainingService.finishedExercisesChanged
+      .takeUntil(this.destroy$)
+      .subscribe((exercises: Exercise[]) => {
+        this.dataSource.data = exercises;
+      });
+    this.trainingService.fetchAllExercises();
   }
 
   ngAfterViewInit() {
@@ -29,6 +36,11 @@ export class PastTrainingComponent implements OnInit, AfterViewInit {
 
   onFilter(filter: string) {
     this.dataSource.filter = filter.trim().toLowerCase();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
   }
 
 }
